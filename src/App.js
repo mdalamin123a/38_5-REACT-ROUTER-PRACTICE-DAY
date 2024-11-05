1cdd430a80bfa7b95aa8b9f0f09525e0910cf152
mdalamin123a <md.alamindev90@gmail.com>
2024-11-05 17:07:54 +0600

    38_5-1-setup-project-and-installation-react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Friend from './components/Friend/Friend';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
+  const [friends, setFriends] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setFriends(data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Friends: {friends.length}</h1>
+      {
+        friends.map(friend => <Friend friend={friend}></Friend>)
+      }
+    </>
   );
 }
 
